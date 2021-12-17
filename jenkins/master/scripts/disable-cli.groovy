6cbc77aea1670d32535eb80bfb9bfc901b999bf3
zhaolei <zhaolei@easyact.cn>
2021-12-17 12:49:37 +0800

    * bugs

--- jenkins/master/scripts/disable-cli.groovy
+++ jenkins/master/scripts/disable-cli.groovy
@@ -2,6 +2,6 @@
 
 import jenkins.model.Jenkins
 
-Jenkins jenkins = Jenkins.getInstance()
-jenkins.CLI.get().setEnabled(false)
+Jenkins jenkins = Jenkins.get()
+jenkins.getDescriptor("jenkins.CLI").get().setEnabled(false)
 jenkins.save()
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -3,12 +3,13 @@ set -xE
 echo "Install Jenkins stable release"
 #yum remove yum-updatesd
 yum remove -y java
-yum install -y java-1.8.0-openjdk wget
+#yum install -y java-1.8.0-openjdk wget
+yum install -y java-11-openjdk-devel wget
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-#sleep 1000
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.el7.x86_64.rpm'
-rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
-yum install -y jenkins
+#curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.el7.x86_64.rpm'
+#rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
+yum upgrade
+yum install -y epel-release jenkins
 chkconfig jenkins on
 #service jenkins start
