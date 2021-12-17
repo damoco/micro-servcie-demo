f714e76dfe82e65f8a871beb7dd17d3d7394c2ba
zhaolei <zhaolei@easyact.cn>
2021-12-17 13:10:43 +0800

    * by github code

--- jenkins/master/setup.sh
+++ jenkins/master/setup.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -xE
-#yum remove -y java
-#yum install -y java-1.8.0-openjdk wget
-#wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-##sleep 1000
-#rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-#curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.el7.x86_64.rpm'
-#rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
-#yum install -y jenkins
-#chkconfig jenkins on
 
+echo "Install git"
 yum install -y git
+
+echo "Setup SSH key"
 mkdir /var/lib/jenkins/.ssh
 touch /var/lib/jenkins/.ssh/known_hosts
 chown -R jenkins:jenkins /var/lib/jenkins/.ssh
@@ -19,9 +13,11 @@ mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
 chmod 600 /var/lib/jenkins/.ssh/id_rsa
 chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
 
+echo "Configure Jenkins"
 mkdir -p /var/lib/jenkins/init.groovy.d
-mv /tmp/*.groovy /var/lib/jenkins/init.groovy.d/
-mv /tmp/jenkins /etc/sysconfig/jenkins
-chmod +x /tmp/install-plugins.sh
-bash /tmp/install-plugins.sh
+mv /tmp/scripts/*.groovy /var/lib/jenkins/init.groovy.d/
+chown -R jenkins:jenkins /var/lib/jenkins/init.groovy.d
+mv /tmp/config/jenkins /etc/sysconfig/jenkins
+chmod +x /tmp/config/install-plugins.sh
+bash /tmp/config/install-plugins.sh
 service jenkins start
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-set -xE
+
+echo "Installing Amazon Linux extras"
+amazon-linux-extras install epel -y
+
 echo "Install Jenkins stable release"
-#yum remove yum-updatesd
 yum remove -y java
+yum install -y java-1.8.0-openjdk
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.el7.x86_64.rpm'
-rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
-yum upgrade
-yum install -y java-11-openjdk wget
 yum install -y jenkins
 chkconfig jenkins on
-#service jenkins start
