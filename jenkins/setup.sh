176a8be1e1f71b1ea9acc1a98cb35ebda08efce8
zhaolei <zhaolei@easyact.cn>
2021-12-17 11:03:29 +0800

    * master jenkins

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -x
+echo "Install Jenkins stable release"
+#yum remove yum-updatesd
 yum remove -y java
 yum install -y java-1.8.0-openjdk wget
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
@@ -9,19 +11,4 @@ curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.e
 rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
 yum install -y jenkins
 chkconfig jenkins on
-
-yum install -y git
-mkdir /var/lib/jenkins/.ssh
-touch /var/lib/jenkins/.ssh/known_hosts
-chown -R jenkins:jenkins /var/lib/jenkins/.ssh
-chmod 700 /var/lib/jenkins/.ssh
-mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
-chmod 600 /var/lib/jenkins/.ssh/id_rsa
-chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
-
-mkdir -p /var/lib/jenkins/init.groovy.d
-mv /tmp/*.groovy /var/lib/jenkins/init.groovy.d/
-mv /tmp/jenkins /etc/sysconfig/jenkins
-chmod +x /tmp/install-plugins.sh
-bash /tmp/install-plugins.sh
 service jenkins start
