835ef785a480e4a95e3dfb51173aa8a014cdb58f
zhaolei <zhaolei@easyact.cn>
2021-12-17 12:59:59 +0800

    * still no demonize

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -2,13 +2,13 @@
 set -xE
 echo "Install Jenkins stable release"
 #yum remove yum-updatesd
-yum remove -y java
+yum remove -y java epel-release
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 #curl -LO 'https://rpmfind.net/linux/epel/7/x86_64/Packages/d/daemonize-1.7.7-1.el7.x86_64.rpm'
 #rpm -Uvh ./daemonize-1.7.7-1.el7.x86_64.rpm
 yum upgrade
-yum install -y java-11-openjdk-devel wget epel-release
+yum install -y java-11-openjdk wget epel-release
 yum install -y jenkins
 chkconfig jenkins on
 #service jenkins start
