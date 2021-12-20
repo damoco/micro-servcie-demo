64713a5a3401809d3e760b3887c69cfc1d9730e6
zhaolei <zhaolei@easyact.cn>
2021-12-20 10:46:51 +0800

    + jenkins_master.tf

--- jenkins/terraform/outputs.tf
+++ jenkins/terraform/outputs.tf
@@ -2,7 +2,7 @@ output "bastion" {
   value = aws_instance.bastion.public_ip
 }
 output "ssh-tunnel-4-jenkins" {
-  value = "ssh -L 8080:10.x:8080 ec2-user@${aws_instance.bastion.public_ip}"
+  value = "ssh -f -L 4000:${aws_instance.jenkins_master.private_ip}:22 -i ~/.ssh/cn.pem ec2-user@${aws_instance.bastion.public_ip} sleep 1000"
 }
 
 output "my-ip" {
